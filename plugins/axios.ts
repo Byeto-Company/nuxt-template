bc242b742165de1ecf41001a61cf2f4ce920295d
marzban-dev <marzban.dev@gmail.com>
2025-01-18 23:03:01 +0330

    Updated

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,26 +1,38 @@
 import axiosOriginal from "axios";
+import { useAuth } from "~/composables/api/auth/useAuth";
+import { API_ENDPOINTS } from "~/constants/api-endpoints";
 
 export default defineNuxtPlugin(() => {
-
     const config = useRuntimeConfig();
+    const { token, logout } = useAuth();
 
-    const baseURL = config.public.API_BASE_URL as string | undefined;
-
-    const axios = axiosOriginal.create({ baseURL });
+    const axios = axiosOriginal.create({
+        baseURL: config.public.API_BASE_URL
+    });
 
     axios.interceptors.request.use((config) => {
-        // if (
-        //     !config.url?.includes(API_ENDPOINTS.auth.login) &&
-        //     !config.url?.includes(API_ENDPOINTS.user.sendOtp)
-        // ) {
-        //     const { token } = useServices();
-        //     config.headers.Authorization = `Bearer ${token.value}`;
-        // }
+        if (!config.url?.includes(API_ENDPOINTS.auth.signin)) {
+            config.headers.Authorization = `Bearer ${token.value}`;
+        }
 
         return config;
     });
 
+    axios.interceptors.response.use((response) => {
+        return response;
+    }, function(error) {
+
+        // if (error.status === 401) {
+        //     logout();
+        // }
+
+        return Promise.reject(error);
+    });
+
     return {
-        provide: { axios }
+        provide: {
+            axios
+        }
     };
+
 });
