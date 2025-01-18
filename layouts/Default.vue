90d1bfbc2984c667330fd5180b50d0822a95d8f1
marzban-dev <marzban.dev@gmail.com>
2025-01-18 23:02:45 +0330

    Add auth checking

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,4 +1,67 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+
+// import
+
+import { useAuth } from "~/composables/api/auth/useAuth";
+import useRefreshAuth from "~/composables/api/auth/useRefreshAuth";
+import useVerify from "~/composables/api/auth/useVerify";
+
+// state
+
+const { mutateAsync: refreshAuth } = useRefreshAuth();
+const { token, refreshToken, updateToken, updateRefreshToken, logout } = useAuth();
+const { mutateAsync: verify } = useVerify();
+
+// lifecycle
+
+onServerPrefetch(async () => {
+    if (!!token.value) {
+
+        // 1.1 - token is there
+
+        try {
+
+            await verify({
+                token: token.value
+            });
+
+            // 2.1 - token is valid, finish
+
+        } catch (e) {
+
+            // 2.2 - token is there, but not valid, try to refresh token
+
+            if (!!refreshToken.value) {
+
+                // 3.1 - refresh token is there, try to refresh
+
+                try {
+                    const refreshResponse = await refreshAuth({ refresh: refreshToken.value });
+
+                    // 4.1 - token is refreshed successfully, finish
+
+                    updateToken(refreshResponse.access);
+                    updateRefreshToken(refreshResponse.refresh);
+                } catch (e) {
+
+                    // 4.2 - cant refreshing token, logout
+
+                    logout();
+                }
+            } else {
+
+                // 3.2 - refresh token is not exist, logout
+
+                logout();
+            }
+        }
+    } else {
+
+        // 1.2 - token is not exist, logout
+
+        logout();
+    }
+});
 
 </script>
 
