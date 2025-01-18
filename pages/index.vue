cb19925e52f17f6ad470983af9b27d62ca302711
marzban-dev <marzban.dev@gmail.com>
2025-01-18 23:02:56 +0330

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script lang="ts" setup>
+
+// onServerPrefetch(async () => {
+//     const response = await suspense();
+//
+//     if (response.isError) {
+//         throw createError({
+//             statusCode: 404,
+//             statusMessage: `error : ${response.error.message}`
+//         });
+//     }
+// });
+
+</script>
+
 <template>
     <div class="h-screen flex items-center justify-center w-full bg-slate-50 text-slate-600 text-[72px]">
         Hello World!
