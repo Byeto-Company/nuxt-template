570195ee26f6501d2d6cddd86a8ea00fb8de37c7
marzban-dev <marzban.dev@gmail.com>
2025-02-24 22:17:24 +0330

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,8 @@
 <script lang="ts" setup>
-
-// onServerPrefetch(async () => {
-//     const response = await suspense();
-//
-//     if (response.isError) {
-//         throw createError({
-//             statusCode: 404,
-//             statusMessage: `error : ${response.error.message}`
-//         });
-//     }
-// });
-
 </script>
 
 <template>
-    <div class="h-screen flex items-center justify-center w-full bg-slate-50 text-slate-600 text-[72px]">
+    <div class="h-screen flex items-center justify-center gap-4 flex-col w-full bg-slate-50 text-slate-600 text-[72px]">
         Hello World!
     </div>
 </template>
