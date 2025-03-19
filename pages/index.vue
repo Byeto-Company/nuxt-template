640a05f3c0b2b3c7d9e23cfd558823fea129bd2c
marzban-dev <marzban.dev@gmail.com>
2025-03-19 21:35:32 +0330

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
+
 </script>
 
 <template>
-    <div class="h-screen flex items-center justify-center gap-4 flex-col w-full bg-slate-50 text-slate-600 text-[72px]">
+    <div class="h-screen flex items-center justify-center gap-4 flex-col w-full text-neutral-50 text-[72px]">
         Hello World!
     </div>
 </template>
