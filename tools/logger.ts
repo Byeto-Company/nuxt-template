2287af3eccbc8c28a96ec739f7b60a807f169b7f
marzban-dev <marzban.dev@gmail.com>
2025-03-19 18:49:16 +0330

    Updated

--- tools/logger.ts
+++ tools/logger.ts
@@ -1,73 +1,31 @@
 import fs from "fs/promises";
 
-type LogType = {
-    title: string;
-    status?: "success" | "error" | "info" | "warning";
-    message?: string,
-    details?: any
-}
-
 class Logger {
-    private static formatToMarkdown(log: LogType) {
-        const date = new Date();
-        let month = "" + (date.getMonth() + 1);
-        let day = "" + date.getDate();
-        let year = date.getFullYear();
-        let hour = date.getHours();
-        let minutes = date.getMinutes();
-        let seconds = date.getSeconds();
+    public static async axiosErrorLog(error: any) {
+        const errorJson = error.toJSON();
 
-        if (month.length < 2) {
-            month = "0" + month;
-        }
+        const nowDate = new Date();
 
-        if (day.length < 2) {
-            day = "0" + day;
-        }
-
-        let markdownContent = "";
-
-        let icon = "ℹ️";
-
-        switch (log.status) {
-            case "info":
-                icon = "ℹ️";
-                break;
-            case "error":
-                icon = "‼️";
-                break;
-            case "warning":
-                icon = "⚠️";
-                break;
-            case "success":
-                icon = "✅";
-                break;
-            default :
-                icon = "ℹ️";
-                break;
-        }
-
-        markdownContent += `# ${icon} ${log.title} \n`;
-        markdownContent += `## ${[year, month, day].join("-")} ${hour}:${minutes}:${seconds} \n`;
-
-        if (log.message) {
-            markdownContent += `**Message:** ${log.message}\n`;
-        }
-        if (log.details) {
-            markdownContent += `**Details:**\n\n\`\`\`json\n${JSON.stringify(log.details, null, 2)}\n\`\`\`\n\n`;
-        }
-
-        markdownContent += "<br></br>\n\n";
-        markdownContent += "---\n";
-
-        return markdownContent;
-    }
-
-    public static async log(info: LogType) {
-        const formattedLog = this.formatToMarkdown(info);
+        const logData: AxiosLogType = {
+            url: errorJson.config.url,
+            code: errorJson.code!,
+            status: errorJson.status!,
+            method: errorJson.config.method,
+            response: error?.response?.data,
+            requestHeaders: errorJson.config.headers,
+            responseHeaders: error.response.headers,
+            payload: errorJson.config.data ? JSON.parse(errorJson.config.data) : undefined,
+            params: errorJson.config.params ?? undefined,
+            date: nowDate.toString()
+        };
 
         try {
-            await fs.appendFile(".logs/log.md", formattedLog);
+            const oldLogs = await fs.readFile(".logs/log.json", "utf-8");
+            const oldLogsJson = JSON.parse(oldLogs) as Record<any, any>[];
+
+            oldLogsJson.push(logData);
+
+            await fs.writeFile(".logs/log.json", JSON.stringify(oldLogsJson));
         } catch (e) {
             console.error(e);
         }
