2a569635913ba6df87ca5db1a8cc0350cceb6fc3
marzban-dev <marzban.dev@gmail.com>
2025-04-24 23:24:36 +0330

    Updated

--- modules/2.app-auth/middleware/checkIsNotLoggedIn.ts
+++ modules/2.app-auth/middleware/checkIsNotLoggedIn.ts
@@ -1,11 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
     const { token } = useAuth();
 
-    const runtimeConfig = useRuntimeConfig()
-    const moduleConfig = runtimeConfig.public.appAuth
-
-    console.log(moduleConfig);
-
     if (!token.value) {
         return;
     } else {
