409fd26b6b0c1c334f66a30cc3a7a1e3a7dc2c56
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-06-29 18:55:31 +0330

    new update and create page

---
+++ pages/article-builder/create.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container px-0 min-h-screen border-x-2 border-neutral-800 py-9">
+        <ArticleMainTitleModal />
+        <ArticleHero />
+        <ClientOnly>
+            <ArticleRenderer />
+        </ClientOnly>
+    </div>
+    <ActionButton />
+</template>
