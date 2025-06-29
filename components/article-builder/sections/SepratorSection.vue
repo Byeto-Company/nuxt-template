55a81ea88a91fae7e5c7f10c203c711131043db0
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-06-29 18:55:00 +0330

    added seprator section

---
+++ components/article-builder/sections/SepratorSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// types
+
+type Props = {
+    id: number;
+    content: any;
+};
+
+// props
+
+const props = defineProps<Props>();
+const { id } = toRefs(props);
+</script>
+
+<template>
+    <SectionsWrapper
+        :id="id"
+        title="جدا کننده"
+        :contentElevation="false"
+    />
+</template>
+
+<style scoped></style>
