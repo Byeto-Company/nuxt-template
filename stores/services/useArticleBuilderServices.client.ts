b2ed76713a57764c4724a5d7ae6c933a2f219a46
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-06-29 18:57:25 +0330

    new update for article builder services

---
+++ stores/services/useArticleBuilderServices.client.ts
@@ -0,0 +1,59 @@
+const useArticleBuilderServices = () => {
+    const articleStore = useArticleBuilderStore();
+
+    // computeds
+
+    const articleContent = computed({
+        get: () => articleStore.article.content,
+        set: (value) => (articleStore.article.content = value),
+    });
+
+    // methods
+
+    const appendContent = (value: Omit<ArticleSection, "id">) => {
+        const newConrent = {
+            id: (articleContent.value[articleContent.value.length - 1]?.id ?? 0) + 1,
+            ...value,
+        };
+
+        articleStore.article.content.push(newConrent);
+    };
+
+    const getContent = (id: number) => {
+        return articleContent.value.find((content) => content.id === id)!.contentValue;
+    };
+
+    const updateContent = (id: number, value: any) => {
+        const contentIndex = articleContent.value.findIndex((content) => content.id === id);
+        const copyOfContents = [...articleContent.value];
+        copyOfContents[contentIndex].contentValue = value;
+        articleContent.value = copyOfContents;
+    };
+
+    const getOptions = (id: number) => {
+        return articleContent.value.find((content) => content.id === id)!.options;
+    };
+
+    const updateContentOptions = (id: number, value: any) => {
+        const contentIndex = articleContent.value.findIndex((content) => content.id === id);
+        const copyOfContents = [...articleContent.value];
+        copyOfContents[contentIndex].options = value;
+        articleContent.value = copyOfContents;
+    };
+
+    const removeContent = (id: number) => {
+        articleContent.value = articleContent.value.filter((content) => content.id !== id);
+    };
+
+    return {
+        store: articleStore,
+        appendContent,
+        getContent,
+        updateContent,
+        getOptions,
+        updateContentOptions,
+        removeContent,
+    };
+};
+
+export default useArticleBuilderServices;
