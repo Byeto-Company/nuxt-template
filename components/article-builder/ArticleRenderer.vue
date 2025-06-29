e8f763a817dc1bcb5820a19598ac3342b07746d0
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-06-29 18:53:18 +0330

    new changes

---
+++ components/article-builder/ArticleRenderer.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+// imports
+
+import { Reorder } from "motion-v";
+import useArticleBuilderServices from "~/stores/services/useArticleBuilderServices.client";
+
+// state
+
+const dragControls = useDragControls();
+
+const { store } = useArticleBuilderServices();
+
+const currentContents = computed({
+    get: () => store.article.content,
+    set: (value) => store.setContent(value),
+});
+
+const articleComponents = ["heading", "paragraph", "image", "seprator", "video", "gallery"];
+
+const componentMap = Object.fromEntries(
+    articleComponents.map((key) => [
+        key,
+        defineAsyncComponent(() => import(`~/components/article-builder/sections/${capitalize(key)}Section.vue`)),
+    ])
+);
+
+// methods
+
+const capitalize = (str: string): string => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+</script>
+
+<template>
+    <Reorder.Group
+        axis="y"
+        v-model:values="currentContents"
+        class="flex flex-col gap-10 py-9 container"
+    >
+        <Reorder.Item
+            v-for="(content, i) in currentContents"
+            :data-index="i"
+            :key="content.id"
+            :value="content"
+            class="relative"
+            :dragListener="false"
+        >
+            <component
+                :is="componentMap[content.type]"
+                :id="content.id"
+                :content="content"
+            ></component>
+        </Reorder.Item>
+    </Reorder.Group>
+</template>
