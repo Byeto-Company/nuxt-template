c705775ba3aa2277e10ac0ab3f86c9b8a962eac8
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-02 23:56:30 +0330

    added delete logic for image preview

--- components/global/ImagePreview.vue
+++ components/global/ImagePreview.vue
@@ -2,34 +2,91 @@
 // imports
 
 import { useImage } from "@vueuse/core";
+import useDeleteFile from "~/composables/api/article-builder/useDeleteFile";
 
 // types
 
 type Props = {
+    id?: number;
     src: string;
     alt?: string;
 };
 
+type Emits = {
+    delete: [id: number];
+};
+
+// props
+
 const props = defineProps<Props>();
 
-const { src } = toRefs(props);
+const { src, id } = toRefs(props);
+
+// emits
+
+const emit = defineEmits<Emits>();
 
 // state
 
 const previewVisible = ref(false);
 
+const toast = useToast();
+
 const { isLoading } = useImage({ src: src.value });
+
+// queries
+
+const { mutateAsync: deleteFile, isPending: deleteFileIsPending } = useDeleteFile();
+
+// methods
+
+const handleDeleteFile = async () => {
+    deleteFile(
+        { id: id.value! },
+        {
+            onSuccess: () => {
+                emit("delete", id.value!);
+                toast.add({
+                    title: "فایل با موفقیت حذف شد",
+                    color: "success",
+                });
+            },
+            onError: () => {
+                toast.add({
+                    title: "خطایی در حذف فایل رخ داد",
+                    color: "error",
+                });
+            },
+        }
+    );
+};
 </script>
 
 <template>
-    <div class="w-full">
+    <div
+        class="w-full"
+        :class="!!id ? 'relative' : ''"
+    >
+        <UButton
+            v-if="!!id"
+            color="error"
+            variant="outline"
+            class="absolute -left-2 -top-2 z-4 aspect-square bg-red-950 hover:bg-red-900 scale-90"
+            @click="handleDeleteFile"
+            :disabled="deleteFileIsPending"
+        >
+            <UIcon
+                :name="deleteFileIsPending ? 'svg-spinners:180-ring' : 'lucide:trash-2'"
+                class="text-xl"
+            />
+        </UButton>
         <USkeleton
             v-if="isLoading"
-            class="w-full h-[30rem] rounded-2xl"
+            class="w-full h-max rounded-2xl"
         />
         <div
             v-else
-            class="relative w-full h-[30rem] group rounded-xl lg:rounded-2xl overflow-hidden"
+            class="relative w-full h-max group rounded-2xl overflow-hidden"
         >
             <slot name="thumbnail" />
 
