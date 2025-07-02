c9371d622d853b8170957fc9837e7b75f3bf78ad
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-02 23:57:42 +0330

    added use app services

---
+++ stores/services/useAppServices.ts
@@ -0,0 +1,17 @@
+const useAppServices = () => {
+    const appStore = useAppStore();
+
+    // methods
+
+    const clearTokens = () => {
+        appStore.setToken("");
+        appStore.setRefreshToken("");
+    };
+
+    return {
+        store: appStore,
+        clearTokens,
+    };
+};
+
+export default useAppServices;
