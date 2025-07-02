d239f83bb536baf9a596dc234b86202298fb939b
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-02 23:56:37 +0330

    added use delete file

---
+++ composables/api/article-builder/useDeleteFile.ts
@@ -0,0 +1,28 @@
+// imports
+
+import { useMutation } from "@tanstack/vue-query";
+import { API_ENDPOINTS } from "~/constants/api-endpoints";
+
+// types
+
+export type DeleteFileRequest = {
+    id: number;
+};
+
+// composable
+
+const useDeleteFile = () => {
+    const { $axios: axios } = useNuxtApp();
+
+    const handleDeleteFile = async ({ id }: DeleteFileRequest) => {
+        const { data } = await axios.delete(`${API_ENDPOINTS.artilce.delete}/${id}`);
+
+        return data;
+    };
+
+    return useMutation({
+        mutationFn: (data: DeleteFileRequest) => handleDeleteFile(data),
+    });
+};
+
+export default useDeleteFile;
