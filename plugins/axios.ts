bc24da905fd6ef3bf3a040d4a5327b8c5503c180
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-03 12:36:48 +0330

    added token to axios headers

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,9 +1,10 @@
 import axiosOriginal from "axios";
 import { API_ENDPOINTS } from "~/constants/api-endpoints";
+import useAppServices from "~/stores/services/useAppServices";
 
 export default defineNuxtPlugin(() => {
     const config = useRuntimeConfig();
-    const { token } = useAuth();
+    const { store } = useAppServices();
 
     const axios = axiosOriginal.create({
         baseURL: config.public.API_BASE_URL,
@@ -13,7 +14,7 @@ export default defineNuxtPlugin(() => {
         const { currentLanguage } = useArticleBuilderStore();
 
         if (!config.url?.includes(API_ENDPOINTS.user.signin)) {
-            config.headers.Authorization = token.value ? `Bearer ${token.value}` : undefined;
+            config.headers.Authorization = store.token ? `Bearer ${store.token}` : undefined;
         }
 
         // @ts-ignore
