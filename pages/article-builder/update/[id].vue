28a5272007f4995f365e9a0cb352a48bedb31aad
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-07 13:23:12 +0330

    removed id page for article

--- pages/article-builder/update/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="container px-0 min-h-screen border-x-2 border-neutral-800 py-9">
-        <ArticleHero />
-        <ClientOnly>
-            <ArticleRenderer />
-        </ClientOnly>
-    </div>
-    <ActionButton />
-</template>
