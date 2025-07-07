b12d39f257b5504f434fab41795006bd7abfa480
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-07 13:11:50 +0330

    added progress bar for inital loading

--- components/article-builder/ArticleRenderer.vue
+++ components/article-builder/ArticleRenderer.vue
@@ -2,20 +2,23 @@
 // imports
 
 import { Reorder } from "motion-v";
+import useGetArticle from "~/composables/api/article-builder/useGetArticle";
 import useArticleBuilderServices from "~/stores/services/useArticleBuilderServices";
 
 // state
 
 const dragControls = useDragControls();
 
+const progress = ref(null);
+
 const { store } = useArticleBuilderServices();
 
 const currentContents = computed({
-    get: () => store.article.content,
-    set: (value) => store.setContent(value),
+    get: () => store.article.contents,
+    set: (value) => store.setContents(value),
 });
 
-const articleComponents = ["heading", "paragraph", "image", "seprator", "video", "gallery", "attachment"];
+const articleComponents = ["heading", "paragraph", "image", "separator", "video", "gallery", "attachment"];
 
 const componentMap = Object.fromEntries(
     articleComponents.map((key) => [
@@ -24,19 +27,43 @@ const componentMap = Object.fromEntries(
     ])
 );
 
+// queries
+
+const { data: article, isPending: GetArticleIsPending, isSuccess: GetArticleIsSuccess } = useGetArticle();
+
 // methods
 
 const capitalize = (str: string): string => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+whenever(
+    () => GetArticleIsSuccess.value,
+    async (nv) => {
+        if (nv) {
+            await Object.assign(store.article, article.value);
+        }
+    }
+);
 </script>
 
 <template>
+    <Transition name="fade">
+        <UProgress
+            v-if="GetArticleIsPending"
+            v-model="progress"
+            class="fixed top-0 inset-x-0"
+            :ui="{
+                base: '!h-1 !bg-neutral-800/20 rounded-none',
+            }"
+        />
+    </Transition>
+
     <Reorder.Group
         as="ol"
         axis="y"
         v-model:values="currentContents"
-        class="flex flex-col gap-10 py-9 container"
+        class="flex flex-col gap-9 !px-5 !py-9 container"
     >
         <Reorder.Item
             v-for="(content, i) in currentContents"
@@ -47,7 +74,7 @@ const capitalize = (str: string): string => {
             class="relative"
         >
             <component
-                :is="componentMap[content.type]"
+                :is="componentMap[content.content_type]"
                 :id="content.id"
                 :content="content"
             ></component>
