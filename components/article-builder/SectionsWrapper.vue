dba818a15e8d4f478c74f5d0940128f3f74efd01
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-08 12:24:53 +0330

    added section update

--- components/article-builder/SectionsWrapper.vue
+++ components/article-builder/SectionsWrapper.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 // imports
 
+import useDeleteSection from "~/composables/api/article-builder/useDeleteSection";
+import usePatchSection from "~/composables/api/article-builder/usePatchSection";
 import useArticleBuilderServices from "~/stores/services/useArticleBuilderServices";
 
 // type
@@ -11,6 +13,9 @@ type Props = {
     contentElevation?: boolean;
 };
 
+type ContentValue = Pick<ArticleSection, "content_value">;
+type Options = Pick<ArticleSection, "options">;
+
 // props
 
 const props = withDefaults(defineProps<Props>(), {
@@ -20,7 +25,76 @@ const { id } = toRefs(props);
 
 // state
 
-const { removeContent } = useArticleBuilderServices();
+const patchMessage = ref<{
+    title: string;
+    status: "error" | "success" | "primary" | "secondary" | "info" | "warning" | "neutral" | undefined;
+} | null>(null);
+
+const { getContent, getOptions, removeContent } = useArticleBuilderServices();
+
+// computed
+
+const content_value = computed<ContentValue>(() => getContent(id.value));
+
+const options = computed<Options>(() => getOptions(id.value)!);
+
+// queries
+
+const { mutateAsync: patchSection, isPending: patchSectionIsPending } = usePatchSection();
+
+const { mutateAsync: deleteSection, isPending: deleteSectionIsPending } = useDeleteSection();
+
+// methods
+
+const handleDelete = () => {
+    deleteSection(
+        { id: id.value },
+        {
+            onSuccess: () => {
+                removeContent(id.value);
+            },
+        }
+    );
+};
+
+const handlePatch = (content_value: ContentValue, options: Options) => {
+    patchSection(
+        {
+            id: id.value,
+            variables: {
+                content_value: content_value ?? null,
+                options: options ?? null,
+            },
+        },
+        {
+            onSuccess: () => {
+                patchMessage.value = {
+                    title: "ذخیره شده",
+                    status: "success",
+                };
+                setTimeout(() => {
+                    patchMessage.value = null;
+                }, 2000);
+            },
+            onError: () => {
+                patchMessage.value = {
+                    title: "خطا در ذخیره سازی",
+                    status: "error",
+                };
+            },
+        }
+    );
+};
+
+// watch
+
+watchDebounced(
+    () => [content_value.value, options.value],
+    ([nv1, nv2]) => {
+        handlePatch(nv1 as ContentValue, nv2 as Options);
+    },
+    { debounce: 1000, maxWait: 1500, deep: true }
+);
 </script>
 
 <template>
@@ -32,6 +106,39 @@ const { removeContent } = useArticleBuilderServices();
                 {{ title }}
             </span>
             <div class="flex items-center justify-end gap-4">
+                <Transition
+                    name="fade"
+                    mode="out-in"
+                >
+                    <UIcon
+                        v-if="patchSectionIsPending"
+                        name="svg-spinners:180-ring-with-bg"
+                        class="text-2xl"
+                    />
+                    <UBadge
+                        v-else-if="!!patchMessage"
+                        :label="patchMessage.title"
+                        :color="patchMessage.status"
+                        variant="subtle"
+                        trailing-icon="lucide:square-check"
+                        :ui="{
+                            base: 'text-xs py-[9px] gap-2',
+                            trailingIcon: 'text-lg',
+                        }"
+                    />
+                </Transition>
+                <UButton
+                    v-if="!!patchMessage && patchMessage.status == 'error'"
+                    @click="handlePatch(content_value, options)"
+                    color="neutral"
+                    class="p-2"
+                    variant="subtle"
+                >
+                    <UIcon
+                        name="lucide:refresh-ccw"
+                        class="text-xl"
+                    />
+                </UButton>
                 <UButton
                     color="neutral"
                     class="p-2"
@@ -43,13 +150,14 @@ const { removeContent } = useArticleBuilderServices();
                     />
                 </UButton>
                 <UButton
-                    @click="removeContent(id)"
+                    @click="handleDelete"
                     color="error"
                     class="p-2"
+                    :disabled="deleteSectionIsPending"
                     variant="subtle"
                 >
                     <UIcon
-                        name="lucide:trash-2"
+                        :name="deleteSectionIsPending ? 'svg-spinners:180-ring-with-bg' : 'lucide:trash-2'"
                         class="text-xl"
                     />
                 </UButton>
