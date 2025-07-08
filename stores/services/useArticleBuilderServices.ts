749b5fdc822102d1872ed374dee39009f75b658e
Mamalizz <mohammad.hosein.ghorbani82@gmail.com>
2025-07-08 12:26:25 +0330

    fixed article update content

--- stores/services/useArticleBuilderServices.ts
+++ stores/services/useArticleBuilderServices.ts
@@ -21,7 +21,9 @@ const useArticleBuilderServices = () => {
     const updateContent = (id: number, value: any) => {
         const contentIndex = articleContent.value.findIndex((content) => content.id === id);
         if (contentIndex !== -1) {
-            articleContent.value[contentIndex].content_value = value;
+            const updated = [...articleContent.value];
+            updated[contentIndex] = { ...updated[contentIndex], content_value: value };
+            articleContent.value = updated;
         }
     };
 
@@ -32,7 +34,9 @@ const useArticleBuilderServices = () => {
     const updateContentOptions = (id: number, value: any) => {
         const contentIndex = articleContent.value.findIndex((content) => content.id === id);
         if (contentIndex !== -1) {
-            articleContent.value[contentIndex].options = value;
+            const updated = [...articleContent.value];
+            updated[contentIndex] = { ...updated[contentIndex], options: value };
+            articleContent.value = updated;
         }
     };
 
