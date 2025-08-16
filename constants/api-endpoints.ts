bb4eb1a5a187e86e3fed3b08400c35837c1c3b5f
marzban-dev <marzban.dev@gmail.com>
2025-08-16 23:10:55 +0330

    Update constants

--- constants/api-endpoints.ts
+++ constants/api-endpoints.ts
@@ -1,21 +1,32 @@
 export const API_ENDPOINTS = {
     user: {
-        profile: "/user/profile",
-        refresh: "/user/token/refresh",
-        verify: "/user/verify",
-        signin: "/user/token",
-        logout: "/user/logout",
-        otp: "/user/send_otp",
-    },
-    article: {
-        patch_hero: "/article/child",
-        upload_file: "/article/file",
-        delete_file: "/article/file",
-        create_section: "/article/content",
-        delete_section: "/article/content",
-        patch_section: "/article/content",
-        reorder_section: "/article/content/reorder",
-        signin : "/user/token/admin-pannel",
-        get: "/article/child",
+        profile: {
+            key: "",
+            path: "/user/profile",
+        },
+        refresh: {
+            path: "/user/token/refresh",
+            key: "",
+        },
+        verify: {
+            path: "/user/verify",
+            key: "",
+        },
+        signin: {
+            path: "/user/token",
+            key: "",
+        },
+        logout: {
+            path: "/user/logout",
+            key: "",
+        },
+        otp: {
+            path: "/user/send_otp",
+            key: "",
+        },
+        develop_token: {
+            path: "/user/develop_token",
+            key: "",
+        },
     },
 };
--- constants/provide-keys.ts
+++ constants/provide-keys.ts
@@ -1,8 +1,2 @@
 export const PROVIDE_KEYS = {
-    // reservation: {
-    //     SelectedCells: Symbol("SelectedCells") as InjectionKey<{
-    //         set: (value: Record<any, any>) => void;
-    //         current: Ref<Record<any, any>[]>;
-    //     }>,
-    // },
 };
