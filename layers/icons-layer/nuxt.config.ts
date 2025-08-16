761be80daa703ab0c7e9adb86ee01bf9ab3ad781
marzban-dev <marzban.dev@gmail.com>
2025-08-16 23:02:12 +0330

    Update icons layer

--- layers/icons-layer/nuxt.config.ts
+++ layers/icons-layer/nuxt.config.ts
@@ -1,12 +1,8 @@
 export default defineNuxtConfig({
     icon: {
         cssLayer: "base",
-        customCollections: [
-            {
-                prefix: "ci",
-                dir: "layers/icons-layer/public/icons",
-            },
-        ],
+        iconifyApiEndpoint: "https://icon-server.time4reserve.ir/",
+        fallbackToApi: true,
     },
     modules: ["@nuxt/icon"],
 });
