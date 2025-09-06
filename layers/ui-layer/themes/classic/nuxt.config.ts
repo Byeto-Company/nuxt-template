1afeef6e46fb3557ce8454258f6dc24c0105bf74
marzban-dev <marzban.dev@gmail.com>
2025-09-07 01:37:06 +0330

    Update

--- layers/ui-layer/themes/classic/nuxt.config.ts
+++ layers/ui-layer/themes/classic/nuxt.config.ts
@@ -1,8 +1,2 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const currentDir = dirname(fileURLToPath(import.meta.url));
-
 export default defineNuxtConfig({
-    css: [join(currentDir, "./assets/css/tailwind.css")],
 });
